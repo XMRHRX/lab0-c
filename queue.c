006ed44964dd946731e6e67f795c59331a6ad99a
xmrhrx <mrhr12345@gmail.com>
2025-02-26 22:53:20 +0800

    Implement q_free
    Free queue with container_of to ensure no dangling memory
    
    - Test case: 12/100
    
    Change-Id: I5fce984016597634cb0ad3a92ebe7724f9fccea0

--- queue.c
+++ queue.c
@@ -22,7 +22,14 @@ struct list_head *q_new()
 }
 
 /* Free all storage used by queue */
-void q_free(struct list_head *head) {}
+void q_free(struct list_head *head)
+{
+    while (head->next != head) {
+        element_t *e = container_of(head->next, element_t, list);
+        free(e);
+    }
+    free(head);
+}
 
 /* Insert an element at head of queue */
 bool q_insert_head(struct list_head *head, char *s)
