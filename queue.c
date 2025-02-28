96aa45cc47d47f942ca183aacfaf6bf19af6e1bf
xmrhrx <mrhr12345@gmail.com>
2025-03-01 01:03:02 +0800

    Implement insert tail
    Implement insert tail with similar logic like insert head.
    
    Change-Id: I0e5e0e1c95b1f7d7e15f9695bc6d9bb4c8d31693

--- queue.c
+++ queue.c
@@ -63,6 +63,19 @@ bool q_insert_head(struct list_head *head, char *s)
 /* Insert an element at tail of queue */
 bool q_insert_tail(struct list_head *head, char *s)
 {
+    if (!head)
+        return false;
+    element_t *e = malloc(sizeof(element_t));
+    if (!e)
+        return false;
+    int length = strlen(s) + 1;
+    e->value = malloc(sizeof(char) * length);
+    if (!e->value) {
+        free(e);
+        return false;
+    }
+    strncpy(e->value, s, length);
+    list_add_tail(&e->list, head);
     return true;
 }
 
